391f4f804e8ae04939e719a8d14f826e8bc9b233
Chintan2108 <chintanmaniyar@gmail.com>
2018-12-11 20:34:14 +0530

    firebase fetch

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {EmployeeService} from '../shared/employee.service'
-import { AngularFireList} from 'angularfire2/database'
 import { Employee } from '../shared/employee.model';
 
 @Component({
@@ -9,12 +8,23 @@ import { Employee } from '../shared/employee.model';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
-  employeelist: AngularFireList<Employee>
+  employeelist: Employee[];
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
-    this.employeeService.getData();
-    
+    var x = this.employeeService.getData();
+    x.snapshotChanges().subscribe(item =>{
+      this.employeelist = [];
+      item.forEach(element =>{
+        var y = element.payload.toJSON();
+        y["$key"] = element.key;
+        this.employeelist.push(y as Employee);
+      })
+    })
+  }
+
+  onItemClick(emp: Employee){
+    this.employeeService.selectedEmployee = Object.assign({}, emp);
   }
 
 }
